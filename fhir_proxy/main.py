fc9031339441ff1067bc34ec00ea9163babefb9b
Fraser Isbester <fraserisbester@gmail.com>
2023-11-05 22:48:11 -0800

    refactor: remove test endpoints

--- fhir_proxy/main.py
+++ fhir_proxy/main.py
@@ -9,26 +9,9 @@ from fhir_proxy.routers import fhir_api
 
 app = FastAPI()
 
-# Registers the FHIR API router with the application
-app.include_router(fhir_api.router, prefix="/api/v1", tags=["FHIR API"])
-
 @app.get("/")
 async def read_resource():
     return {"status", "ok"}
 
-@app.get("/beers/{typ}")
-@cache.caches(key_pattern="beers:{typ}")
-async def get_beers(typ: str, redis: Redis = Depends(cache.get_redis)):
-    try:
-        r = httpx.get(f"https://api.sampleapis.com/beers/{typ}")
-        r.raise_for_status()
-        return r.json()
-    except httpx.HTTPError as e:
-        raise HTTPException(status_code=e.response.status_code, detail=str(e))
-
-@app.put("/beers/{typ}")
-@cache.invalidates(key_pattern="beers:{typ}")
-async def set_beers(typ: str, data: dict, redis: Redis = Depends(cache.get_redis)):
-    # simulate network delay
-    time.sleep(.2)
-    return {"status": "success", "message": "Beers updated and cache invalidated"}
+# Registers the FHIR API router with the application
+app.include_router(fhir_api.router, prefix="/api/v1", tags=["FHIR API"])
